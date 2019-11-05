2588456bf4270fe47ecbfcc8fde7cef82179f30f
jin0s <chinhle93@gmaill.com>
2019-11-05 00:13:46 -0500

    Added compliant code for R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -21,10 +21,35 @@ public  class R02_XP00_J {
     System.out.println("Output from deleteFile(): ");
   }
 
+   /*
+    * Rule 02. Expressions (EXP)
+    *
+    * Corrected code per:
+    * https://wiki.sei.cmu.edu/confluence/display/java/EXP00-J.+Do+not+ignore+values+returned+by+methods
+    */
+
+  public static void deleteFileCompliant(){
+    File someFile = new File("someFleName.txt");
+    // Do something with someFile
+    String result;
+    if(someFile.delete()){
+      result = "success! filed deleted";
+    }
+    else {
+      result = "failed! filed could not be deleted";
+    }
+    System.out.println("Output from deleteFile(): " + result);
+  }
+
   public static void main(String[] args) {
-    // Using Noncompliant code ignoring values returned by methods resulting in no output or response of the delete status
+    // Using Non-Compliant code ignoring values returned by methods resulting in no output or response of the delete status
+    System.out.println("\nResult of Non-Compliant Code");
     deleteFile();
-  
+
+
+    // Using Compliant code using values returned by methods
+    System.out.println("\nResult of Compliant Code");
+    deleteFileCompliant();
   }
 }
 
