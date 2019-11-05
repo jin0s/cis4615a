c9a7cf1cd03bb3bf8909283a954aba21161ab51d
jin0s <chinhle93@gmaill.com>
2019-11-04 23:48:59 -0500

    Added compliant code for R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -7,16 +7,9 @@
  ******************************************************************************/
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public  class R00_IDS03_J {
-
-    /*
-    * Rule 06. Methods (MET)
-    * Corrected code per:
-    * https://wiki.sei.cmu.edu/confluence/display/java/MET01-J.+Never+use+assertions+to+validate+method+arguments
-    *
-    *Rule 06-Met01
-     */
 
   public static class Logger {
     public void severe(String input) {
@@ -24,29 +17,55 @@ public  class R00_IDS03_J {
     }
   }
 
+  public static String sanitizeInput(String input_str){
+    return Pattern.matches("[A-Za-z0-9]+", input_str) ? input_str : "unauthorized user";
+  }
+
 
   public static void main(String[] args) {
-      // Initialize the state in the data
-      boolean loginSuccessful = false;
-      Logger logger = new Logger();
-      String username = "guest";
-      
-      // Using Noncompliant code without data sanitization
-      System.out.println("Logging in with: " + username);
 
-      if (loginSuccessful) {
-        logger.severe("User login succeeded for: " + username);
-      } else {
-        logger.severe("User login failed for: " + username);
-      }
+    /*
+    * Rule 00. Input Validation and Data Sanitization (IDS)
+    * This noncompliant code example logs untrusted data from an unauthenticated user without data sanitization.
+    */
+    // Initialize the state in the data
+    boolean loginSuccessful = false;
+    Logger logger = new Logger();
+    String username = "guest";
+    
+    // Using Noncompliant code without data sanitization
+    System.out.println("Logging in with: " + username);
 
-      // Problem arises here when you can use a new line to inject false logs
-      username = "guest\nSEVERE: User login succeeded for: administrator";
-      if (loginSuccessful) {
-        logger.severe("User login succeeded for: " + username);
-      } else {
-        logger.severe("User login failed for: " + username);
-      }
+    if (loginSuccessful) {
+      logger.severe("User login succeeded for: " + username);
+    } else {
+      logger.severe("User login failed for: " + username);
+    }
+
+    System.out.println("\nNon-Compliant Code Output");
+
+    // Problem arises here when you can use a new line to inject false logs
+    username = "guest\nSEVERE: User login succeeded for: administrator";
+    if (loginSuccessful) {
+      logger.severe("User login succeeded for: " + username);
+    } else {
+      logger.severe("User login failed for: " + username);
+    }
+
+   /*
+    * Rule 00. Input Validation and Data Sanitization (IDS)
+    *
+    * Corrected code per:
+    * https://wiki.sei.cmu.edu/confluence/display/java/IDS03-J.+Do+not+log+unsanitized+user+input
+    *
+    */
+
+    System.out.println("\nCompliant Code Output");
+    if (loginSuccessful) {
+      logger.severe("User login succeeded for: " + sanitizeInput(username));
+    } else {
+      logger.severe("User login failed for: " + sanitizeInput(username));
+    }
   }
 }
 
