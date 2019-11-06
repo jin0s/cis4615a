afaacea814d714e4152d662fddae5f93a6aa7bea
jin0s <chinhle93@gmaill.com>
2019-11-05 22:46:21 -0500

    added compliant code for R06_Met01_J.java

--- R06_Met01_J.java
+++ R06_Met01_J.java
@@ -24,9 +24,49 @@ public class R06_Met01_J {
       System.out.println("b = " + b);
       c = getAbsAdd(a, b);
       System.out.println("c = " + c);
-
+    
+      // Using Compliant code using values returned by methods
+      System.out.println("\nResult of Compliant Code");
+      a = 17;
+      System.out.println("a = " + a);
+      b = 42;
+      System.out.println("b = " + b); 
+      c = getAbsAddCompliant(a, b);
+      System.out.println("c = " + c);
+      System.out.println("\nResult in an integer over flow");
+      a = Integer.MIN_VALUE; //used for testing secure version
+      System.out.println("a = " + a);
+      b = Integer.MAX_VALUE;
+      System.out.println("b = " + b);
+      try {
+        c = getAbsAddCompliant(a, b);
+        System.out.println("c = " + c);
+      } catch (IllegalArgumentException e) {
+        System.out.println(e);
+        System.out.println("ERROR: INTEGER OVERFLOW ");
+      }
     }
 
+   /*
+    * Rule 06. Methods (MET)
+    * Corrected code per:
+    * https://wiki.sei.cmu.edu/confluence/display/java/MET01-J.+Never+use+assertions+to+validate+method+arguments
+    *
+    *Rule 06-Met01
+    */
+
+    public static int getAbsAddCompliant(int x, int y) {
+      if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+        throw new IllegalArgumentException();
+      }
+      int absX = Math.abs(x);
+      int absY = Math.abs(y);
+      if (absX > Integer.MAX_VALUE - absY) {
+        throw new IllegalArgumentException();
+      }
+      return absX + absY;
+    }
+  
     public static int getAbsAdd(int x, int y) {
         return Math.abs(x) + Math.abs(y);
     }
