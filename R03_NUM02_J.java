c0906a60cb8b44862d8648aadad691b9cf357cc4
jin0s <chinhle93@gmaill.com>
2019-11-05 23:54:21 -0500

    added compliant and non compliant code for R03_NUM02.java

---
+++ R03_NUM02_J.java
@@ -0,0 +1,69 @@
+/******************************************************************************
+ *  Compilation:  javac `R03_NUM02_J.java
+ *  Execution:    java R03_NUM02_J
+ *  
+ ******************************************************************************/
+
+
+public  class R03_NUM02_J {
+
+
+  public static void main(String[] args) {
+    long result;
+    long num1 = 10;
+    long num2 = 2;
+    long num3 = 0;
+
+    // Using Non-Compliant code ignoring values returned by methods resulting in no output or response of the delete status
+    System.out.println("\nResult of Non-Compliant Code");
+    result = divide(num1,num2);
+    System.out.printf("%d / %d = %d\n", num1, num2, result);
+
+    try {
+      result = divide(num1,num3);
+      
+    } catch (ArithmeticException e) {
+      System.out.println(e);
+      result = Integer.MAX_VALUE;
+    }
+    System.out.printf("%d / %d = %d\n", num1, num3, result);
+
+
+    // Using Compliant code using values returned by methods
+    System.out.println("\nResult of Compliant Code");
+    result = divideCompliant(num1,num2);
+    System.out.printf("%d / %d = %d\n", num1, num2, result);
+
+    try {
+      result = divideCompliant(num1,num3);
+      
+    } catch (ArithmeticException e) {
+      System.out.println(e);
+      result = Integer.MAX_VALUE;
+    }
+    System.out.printf("%d / %d = %d\n", num1, num3, result);
+
+  }
+
+  public static long divide(long num1, long num2) throws ArithmeticException{
+    return num1 / num2;
+  }
+
+  public static long divideCompliant(long num1, long num2) {
+    if(num2 == 0) {
+      System.out.println("ERROR: Divide by zero");
+      // Setting to zero for the stablilty of the program but catch this error and do something with it
+      return 0;
+    }
+    return num1 / num2;
+  }
+}
+
+
+     /*
+      * Rule 03. Numeric Type and Operations(NUM)
+      * Ensure that division and remainder operations do not result in divide-by-zero errors
+      */
+
+
+
